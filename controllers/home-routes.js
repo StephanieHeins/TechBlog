81c0453dfc7cf4c49d363c660936c3242087b60f
Steph Heins <stephanie_heins@yahoo.com>
2021-11-18 14:23:03 -0800

    fixed edit/delete errors

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -112,4 +112,51 @@ router.get("/createpost", (req, res) => {
     })
 });
 
+router.get("/editpost/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'date_created',
+      'content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_content', 'post_id', 'user_id', 'date_created'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('editpost', {
+          post,
+          loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
